8c7c50804ecf06c91c5f9140c0d1f577143b3361
Jim <gcobcqwe@yahoo.com.tw>
2017-05-05 10:49:20 +0800

    [# Jim]heroku deploy

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+   socket.on('spo', function(msg){
+    io.emit('spo', msg);
+  });
 });
 
 http.listen(port, function(){
